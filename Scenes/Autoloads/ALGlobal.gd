8be1eaab74143e0d9f2047a13d2bafcb7fdc4259
kvbc <mateusz.les222@gmail.com>
2024-01-30 19:59:28 +0100

    test

--- Scenes/Autoloads/ALGlobal.gd
+++ Scenes/Autoloads/ALGlobal.gd
@@ -2,6 +2,9 @@ extends Node
 
 var World: World
 
+func _ready():
+	get_tree().debug_collisions_hint = true
+
 func PlayAudio(stream: AudioStream):
 	var audio_player = AudioStreamPlayer.new()
 	audio_player.autoplay = true
